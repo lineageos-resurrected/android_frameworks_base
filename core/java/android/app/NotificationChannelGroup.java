ee0fca0e61478f549c1866e35032e6804194b77d
Julia Reynolds <juliacr@google.com>
2022-08-19 09:54:23 -0400

    Limit the size of NotificationChannel and NotificationChannelGroup
    Test: android.app.NotificationChannelGroupTest
    Test: android.app.NotificationChannelTest
    Test: cts NotificationChannelTest
    Test: cts NotificationChannelGroupTest
    Bug: 241764350
    Bug: 241764340
    Bug: 241764135
    Bug: 242702935
    Bug: 242703118
    Bug: 242703202
    Bug: 242702851
    Bug: 242703217
    Bug: 242703556
    Change-Id: I0925583ab54d6c81c415859618f6b907ab7baada
    Merged-In: I0925583ab54d6c81c415859618f6b907ab7baada
    (cherry picked from commit 3850857cb0e7f26702d5bd601731d7290390fa3b)
    (cherry picked from commit c2d264989a2c18af9e3f210f62eba8d987fefb5b)
    Merged-In: I0925583ab54d6c81c415859618f6b907ab7baada

--- core/java/android/app/NotificationChannelGroup.java
+++ core/java/android/app/NotificationChannelGroup.java
@@ -37,8 +37,9 @@ public final class NotificationChannelGroup implements Parcelable {
     /**
      * The maximum length for text fields in a NotificationChannelGroup. Fields will be truncated at
      * this limit.
+     * @hide
      */
-    private static final int MAX_TEXT_LENGTH = 1000;
+    public static final int MAX_TEXT_LENGTH = 1000;
 
     private static final String TAG_GROUP = "channelGroup";
     private static final String ATT_NAME = "name";
@@ -68,7 +69,7 @@ public final class NotificationChannelGroup implements Parcelable {
      */
     protected NotificationChannelGroup(Parcel in) {
         if (in.readByte() != 0) {
-            mId = in.readString();
+            mId = getTrimmedString(in.readString());
         } else {
             mId = null;
         }
@@ -91,7 +92,7 @@ public final class NotificationChannelGroup implements Parcelable {
         } else {
             dest.writeByte((byte) 0);
         }
-        TextUtils.writeToParcel(mName, dest, flags);
+        TextUtils.writeToParcel(mName.toString(), dest, flags);
         dest.writeParcelableList(mChannels, flags);
     }
 
