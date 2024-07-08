e876bf4654e043fff871960fe641fa2e1d28c648
Nikhil Kumar <nykkumar@google.com>
2024-07-08 17:01:09 +0100

    [BACKPORT] Systemservice change to propagate userId to MMSService
    Convey the originating user ID across the nested binder calls to MMSService to validate the contentURI is coming from the same userId.
    For detailed design please follow: go/mms-failure-multiuser
    
    Backport: Adapt SmsManager.java, too.
    
    Bug: 350760118
    Test: manually verfied the functionalities after the change (1) Incoming and outgoing SMS/MMS (2) Incoming and outgoing Voice calls
    Flag: EXEMPT this involves changing the method signature in aidl with flags its not possible currently as aidl doesn't support method overloading
    DISABLE_TOPIC_PROTECTOR
    Do-Not-Cherry-Pick: True
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:a3b3e5279a80ddef3c24e1774d395fc0073cbb4c
    Merged-In: I9beb19f1a94aae76e6cd48a72feeb6c1dc47cf5a
    Change-Id: I9beb19f1a94aae76e6cd48a72feeb6c1dc47cf5a

--- telephony/java/android/telephony/SmsManager.java
+++ telephony/java/android/telephony/SmsManager.java
@@ -27,6 +27,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.BaseBundle;
+import android.os.Binder;
 import android.os.Bundle;
 import android.os.RemoteException;
 import android.os.ServiceManager;
@@ -1535,8 +1536,8 @@ public final class SmsManager {
                 return;
             }
 
-            iMms.sendMessage(getSubscriptionId(), ActivityThread.currentPackageName(), contentUri,
-                    locationUrl, configOverrides, sentIntent);
+            iMms.sendMessage(getSubscriptionId(), getCallingUserId(), ActivityThread.currentPackageName(),
+                    contentUri, locationUrl, configOverrides, sentIntent);
         } catch (RemoteException e) {
             // Ignore it
         }
@@ -1569,8 +1570,8 @@ public final class SmsManager {
                 return;
             }
             iMms.downloadMessage(
-                    getSubscriptionId(), ActivityThread.currentPackageName(), locationUrl,
-                    contentUri, configOverrides, downloadedIntent);
+                    getSubscriptionId(), getCallingUserId(), ActivityThread.currentPackageName(),
+                    locationUrl, contentUri, configOverrides, downloadedIntent);
         } catch (RemoteException e) {
             // Ignore it
         }
@@ -1647,7 +1648,7 @@ public final class SmsManager {
         try {
             IMms iMms = IMms.Stub.asInterface(ServiceManager.getService("imms"));
             if (iMms != null) {
-                return iMms.importMultimediaMessage(ActivityThread.currentPackageName(),
+                return iMms.importMultimediaMessage(getCallingUserId(), ActivityThread.currentPackageName(),
                         contentUri, messageId, timestampSecs, seen, read);
             }
         } catch (RemoteException ex) {
@@ -1794,8 +1795,8 @@ public final class SmsManager {
         try {
             IMms iMms = IMms.Stub.asInterface(ServiceManager.getService("imms"));
             if (iMms != null) {
-                return iMms.addMultimediaMessageDraft(ActivityThread.currentPackageName(),
-                        contentUri);
+                return iMms.addMultimediaMessageDraft(getCallingUserId(),
+                        ActivityThread.currentPackageName(), contentUri);
             }
         } catch (RemoteException ex) {
             // ignore it
@@ -2083,4 +2084,11 @@ public final class SmsManager {
         return filtered;
     }
 
+    /**
+     * Retrieves the  calling user id.
+     * @return The id of the calling user.
+     */
+    private int getCallingUserId() {
+        return Binder.getCallingUserHandle().getIdentifier();
+    }
 }
